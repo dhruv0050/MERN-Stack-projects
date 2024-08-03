c1386832f91f6f53718c1be3dba6906657725aeb
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2024-08-03 22:24:25 +0530

    Add files via upload

---
+++ Thoughts/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/app")
+
+const userSchema = mongoose.Schema({
+    username : String,
+    name : String,
+    email : String,
+    age : Number,
+    password : String,
+    image : {
+        type : String,
+        default : "default.jpg"
+    },
+    posts : [{type:mongoose.Schema.Types.ObjectId , ref : "post"}]
+})
+
+module.exports = mongoose.model('user', userSchema); 
---
+++ Thoughts/public/javascripts/additionals.js
@@ -0,0 +1,3 @@
+document.querySelector(".pfp").addEventListener("click", (e)=>{
+    window.location.href = "/profile/upload";
+})
